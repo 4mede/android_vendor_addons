3aa4c73b010069ef58b7a6668f2c53ac34802405
Vhmit <viktor.9630@protonmail.com>
2025-03-06 08:05:46 -0300

    addons: Init repo added more fonts
    Change-Id: Ib4a93d14d3d5cfc650e0e77f3ff9e15ce100586d
    Co-authored-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

---
+++ fonts.mk
@@ -0,0 +1,15 @@
+# Fonts    
+PRODUCT_PACKAGES += \
+    FontGoogleSansOverlay \
+    FontGoogleSansLatoOverlay \
+    FontHarmonySansOverlay \
+    FontInterOverlay \
+    FontOneplusSlateOverlay \
+    FontOppoSansOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontUbuntuOverlay
+
+# Register vendor fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/addons/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/addons/permissions/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
